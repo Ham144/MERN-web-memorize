9be2f11b4e0c1576f4a1b94bf123333e744fb2fe
ham144 <24434muhammad.yafizham@gmail.com>
2024-01-19 22:31:06 +0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import mongoose from 'mongoose'
+const app = express()
+
+app.route('/')
+.get('/home', ())
+
+app.listen(2002, ()=> console.log(`SERVER IS UP`))
